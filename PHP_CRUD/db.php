e26bfdb91193f74f0b1ca4fbe89684de4877be27
Peter Justin Delos Reyes <151437845+peter123pogi@users.noreply.github.com>
2024-10-23 23:41:00 +0800

    Update db.php

--- PHP_CRUD/db.php
+++ PHP_CRUD/db.php
@@ -3,12 +3,12 @@ $con = new mysqli('localhost', 'root', '', 'delosreyes_crud');
 
 /*
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(22),
     age INT,
     sex ENUM('Male', 'Female'),
     email VARCHAR(299) NOT NULL DEFAULT 'No Info',
     contact_number VARCHAR(299) NOT NULL DEFAULT 'No Info',
-    created_at TIMESTAMPS NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 */
